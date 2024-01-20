ee2b218bd19e4a52520674f99bce1d9d58ceba6c
Manish <113422288+manishk1109@users.noreply.github.com>
2024-01-20 14:13:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+let no = document.querySelector(".noBtn");
+let yes = document.querySelector(".yesBtn");
+
+let x;
+let y;
+
+no.addEventListener('click',()=>{
+     x = 5+  Math.random()*100;
+     y = 10+ Math.random()*200;
+    no.style.right = x + "px";
+    no.style.bottom = y + "px";
+    
+})
+
+yes.addEventListener('click',()=>{
+    let please = document.querySelector('.middleimg');
+    let title = document.querySelector('.title');
+    please.src = 'thanks.gif';
+        title.innerHTML = 'Thanks💖✨' ;
+    document.querySelectorAll('.')
+})
